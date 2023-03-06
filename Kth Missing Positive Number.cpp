f604c3dd9d3d9f28dfaeb948140d52396ba35164
Jash Hinger <93431632+jash459@users.noreply.github.com>
2023-03-06 09:15:40 +0530

    Add files via upload

---
+++ Kth Missing Positive Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+       int low = 0;
+       int high = arr.size()-1 ;
+       int mid;
+       while (low <= high) {
+            mid = (low + high) / 2;
+            if (arr[mid] - (1 + mid) < k)  //A[m]-(m+1) - This gives umber of missing number before i'th index
+                low = mid + 1;
+            else
+                high = mid-1;
+        }
+        return low + k;
+    }
+};
