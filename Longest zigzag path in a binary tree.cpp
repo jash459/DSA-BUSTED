dfc683e66c9cc43bb1a5cd574e154fb5f44c68e0
Jash Hinger <93431632+jash459@users.noreply.github.com>
2023-04-19 07:52:03 +0530

    Update Longest zigzag path in a binary tree.cpp

--- Longest zigzag path in a binary tree.cpp
+++ Longest zigzag path in a binary tree.cpp
@@ -1,46 +1,33 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    int MAX;
-    unordered_map<TreeNode*, vector<int>> dp;
 public:
-    int longestZigZag(TreeNode* root) {
-        dp.clear();
-        dfs(root);
-        return MAX;
+    int res = 0;
+
+    void solve(TreeNode* node, bool goLeft, int currPathLen) {
+        if (!node) return;
+        res = max(res, currPathLen);
+        if (goLeft) {
+            solve(node->left, false, currPathLen + 1);
+            solve(node->right, true, 1);
+        } else {
+            solve(node->right, true, currPathLen + 1);
+            solve(node->left, false, 1);
+        }
     }
 
-    void dfs(TreeNode* root) {
-        if (!root) return;
-
-        if (!root->left && !root->right) {
-            dp[root] = {0, 0};
-            return;
-        }
-
-        dfs(root->left);
-        dfs(root->right);
-
-        int left = 0, right = 0;
-
-        if (dp.find(root->left) != dp.end())
-            left = 1 + dp[root->left][1];
-
-        if (dp.find(root->right) != dp.end())
-            right = 1 + dp[root->right][0];
-
-        dp[root] = {left, right};
-        MAX = max(MAX, max(left, right));
+    int longestZigZag(TreeNode* root) {
+        solve(root, true, 0);
+        solve(root, false, 0);
+        return res;
     }
 };
