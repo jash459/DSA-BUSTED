53945b1686386448f548f4c9d23381c33f79ea19
Jash Hinger <93431632+jash459@users.noreply.github.com>
2023-04-22 09:15:48 +0530

    Add files via upload

---
+++ Minimum insertion step to make string palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int fun(string& s1, string& s2, int ind1, int ind2,  vector<vector<int>>& dp){
+
+    if(ind1<0 || ind2<0)
+        return 0;
+        
+    if(dp[ind1][ind2]!=-1)
+        return dp[ind1][ind2];
+    
+    if(s1[ind1] == s2[ind2])
+        return dp[ind1][ind2] = 1 + fun(s1,s2,ind1-1,ind2-1,dp);
+    
+    else 
+        return dp[ind1][ind2] = 0 + max(fun(s1,s2,ind1,ind2-1,dp),fun
+       (s1,s2,ind1-1,ind2,dp));
+   
+}
+
+    int lcs(string s1, string s2) {
+    int n=s1.size();
+    int m=s2.size();
+
+    vector<vector<int>> dp(n,vector<int>(m,-1));
+    return fun(s1,s2,n-1,m-1,dp);
+}
+    int minInsertions(string s) {
+          string t = s;
+        reverse(s.begin(),s.end());
+        return s.size()-lcs(s,t);
+    }
+};
