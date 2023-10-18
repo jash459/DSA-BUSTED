f744b7b23b050de1804d72f14efad696223390bd
Jash Hinger <93431632+jash459@users.noreply.github.com>
2023-10-18 07:36:24 +0530

    Create Parallel Courses III.cpp

---
+++ Parallel Courses III.cpp
@@ -0,0 +1,62 @@
+// User function Template for C++
+
+/*
+Time Complexity: O(V+E)+O(N*logN), where V = no. of nodes and E = no. of edges. This is a simple BFS algorithm. Extra O(N*logN) for sorting the safeNodes array, where N is the number of safe nodes.
+
+Space Complexity: O(N) + O(N) + O(N) ~ O(3N), O(N) for the indegree array, O(N) for the queue data structure used in BFS(where N = no.of nodes), and extra O(N) for the adjacency list to store the graph in a reversed order.
+
+*/
+// anyone who are part of cycle 
+// and anyone leads to cycle(connected ot cycle) are not considered as safe node
+// safe node- > every path ends on terminal node(means the node should have 0 outgoing degree, no incoming edges to it)
+// here we are using Detect a cycle using DFS in directed graph.
+// you will only reset the path visited if and only if you didnt get the cycle
+// to be a part of cycle its has to visited and path visited and you break , you return
+
+
+class Solution {
+private:
+	bool dfsCheck(int node, vector<int> adj[], int vis[],
+ int pathVis[], 
+        int check[]) {
+		vis[node] = 1;
+		pathVis[node] = 1;
+		check[node] = 0;
+		// traverse for adjacent nodes
+		for (auto it : adj[node]) {
+			// when the node is not visited
+			if (!vis[it]) {
+			if (dfsCheck(it, adj, vis, pathVis, check) == true) {
+					check[node] = 0;
+					return true;
+				}
+
+			}
+			// if the node has been previously visited
+			// but it has to be visited on the same path
+			else if (pathVis[it]) {
+				check[node] = 0;
+				return true;
+			}
+		}
+		check[node] = 1;
+		pathVis[node] = 0;
+		return false;
+	}
+public:
+	vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+		int vis[V] = {0};
+		int pathVis[V] = {0};
+		int check[V] = {0};
+		vector<int> safeNodes;
+		for (int i = 0; i < V; i++) {
+			if (!vis[i]) {
+				dfsCheck(i, adj, vis, pathVis, check);
+			}
+		}
+		for (int i = 0; i < V; i++) {
+			if (check[i] == 1) safeNodes.push_back(i);
+		}
+		return safeNodes;
+	}
+};
