e766c6d4f091c1ff680bbb66748fe9f82127f950
Jash Hinger <93431632+jash459@users.noreply.github.com>
2024-02-01 08:54:32 +0530

    Create Divide Array Into Arrays With Max Difference.cpp

---
+++ Divide Array Into Arrays With Max Difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for (int i=0; i<=n-3; i+=3)
+        {
+            if (nums[i+2]-nums[i+1] <= k && nums[i+1]-nums[i] <= k && nums[i+2]-nums[i] <= k)
+                res.push_back({nums[i], nums[i+1], nums[i+2]});
+            else
+                return {};
+        }
+
+        return res;
+    }
+};
