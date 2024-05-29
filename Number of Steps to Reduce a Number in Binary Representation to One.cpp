95663446ef657ef8db2bd7fa53547a201c110422
Jash Hinger <93431632+jash459@users.noreply.github.com>
2024-05-29 07:45:03 +0530

    Create Number of Steps to Reduce a Number in Binary Representation to One.cpp

---
+++ Number of Steps to Reduce a Number in Binary Representation to One.cpp
@@ -0,0 +1,11 @@
+int numSteps(string &s) {
+    int res = 0, carry = 0;
+    for (auto i = s.size() - 1; i > 0; --i) {
+        ++res;
+        if (s[i] - '0' + carry == 1) {
+            carry = 1;
+            ++res;
+        }
+    }
+    return res + carry;
+}
