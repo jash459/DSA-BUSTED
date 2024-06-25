bc535bad69842637b88173826de00a050de6e580
Jash Hinger <93431632+jash459@users.noreply.github.com>
2024-06-25 12:01:53 +0530

    Create Binary Search Tree to Greater Sum Tree.cpp

---
+++ Binary Search Tree to Greater Sum Tree.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    int sum = 0;
+
+    void traverse(TreeNode* root) {
+        if (root) {
+            traverse(root->right);  
+            sum += root->val; 
+            root->val = sum;  
+            traverse(root->left); 
+        }
+    }
+
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        traverse(root);
+        return root;
+    }
+};
