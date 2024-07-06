af8cd92eea998ba534f9bd21f6d816ae4514ba9c
Jash Hinger <93431632+jash459@users.noreply.github.com>
2024-07-06 08:07:48 +0530

    Create Pass the Pillow.cpp

---
+++ Pass the Pillow.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int passThePillow(int n, int k) {
+        int ballPosition = 0; // Start with child 0 holding the ball
+        int direction = 1;    // Initially, the ball moves towards the right
+
+        for (int i = 0; i < k; i++) {
+            if (direction == 1) {
+                if (ballPosition == n - 1)
+                    direction = -1;
+                ballPosition += direction;
+            } else {
+                if (ballPosition == 0)
+                    direction = 1;
+                ballPosition += direction;
+            }
+        }
+        return ballPosition + 1;
+    }
+};
