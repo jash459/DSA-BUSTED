b59eb2ca5ec3e9b8c7c02e01acd3a7160a8f0bb5
Jash Hinger <93431632+jash459@users.noreply.github.com>
2024-08-08 12:43:16 +0530

    Create Spiral Matrix III.cpp

---
+++ Spiral Matrix III.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool init = []() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        return true;
+    }();
+    
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int x, int y) {
+        vector<vector<int>> ans;
+        int total = rows * cols, count = 0, width = 1, height = 1;
+        
+        while (count < total) {
+            // Move right
+            for (int i = 0; i < width; ++i) {
+                if (x >= 0 && x < rows && y >= 0 && y < cols) {
+                    ans.push_back({x, y});
+                    ++count;
+                }
+                ++y;
+            }
+            ++width;
+            
+            // Move down
+            for (int i = 0; i < height; ++i) {
+                if (x >= 0 && x < rows && y >= 0 && y < cols) {
+                    ans.push_back({x, y});
+                    ++count;
+                }
+                ++x;
+            }
+            ++height;
+            
+            // Move left
+            for (int i = 0; i < width; ++i) {
+                if (x >= 0 && x < rows && y >= 0 && y < cols) {
+                    ans.push_back({x, y});
+                    ++count;
+                }
+                --y;
+            }
+            ++width;
+            
+            // Move up
+            for (int i = 0; i < height; ++i) {
+                if (x >= 0 && x < rows && y >= 0 && y < cols) {
+                    ans.push_back({x, y});
+                    ++count;
+                }
+                --x;
+            }
+            ++height;
+        }
+        
+        return ans;
+    }
+};
