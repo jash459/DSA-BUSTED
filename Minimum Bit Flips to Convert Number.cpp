5a7e72a026f811818bdcb51c12788fdf10935c49
Jash Hinger <93431632+jash459@users.noreply.github.com>
2024-09-11 08:51:28 +0530

    Create Minimum Bit Flips to Convert Number.cpp

---
+++ Minimum Bit Flips to Convert Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int ans = start ^ goal;
+        int cnt = 0;
+        
+        while(ans != 0){
+            ans = ans & (ans -1);
+            cnt = cnt +1;
+        }
+        
+        return cnt;
+    }
+};
