09817b5f5d1f6822effebf299bd0c8d35b6f1bf9
Jash Hinger <93431632+jash459@users.noreply.github.com>
2025-02-21 10:29:29 +0530

    Create Find Elements in a Contaminated Binary Tree.cpp

---
+++ Find Elements in a Contaminated Binary Tree.cpp
@@ -0,0 +1,28 @@
+class FindElements {
+public:
+    unordered_map<int, bool> mp;
+
+    void recover(TreeNode* root) {
+        if (!root) return;
+        if (root->left) {
+            root->left->val = root->val * 2 + 1;
+            mp[root->left->val] = true;
+            recover(root->left);
+        }
+        if (root->right) {
+            root->right->val = root->val * 2 + 2;
+            mp[root->right->val] = true;
+            recover(root->right);
+        }
+    }
+
+    FindElements(TreeNode* root) {
+        root->val = 0;
+        mp[0] = true;
+        recover(root);
+    }
+    
+    bool find(int target) {
+        return mp.find(target) != mp.end();
+    }
+};
