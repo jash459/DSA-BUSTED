872705dd444923715415483f3d110a2616c9bc4e
Jash Hinger <93431632+jash459@users.noreply.github.com>
2025-04-11 07:33:50 +0530

    Create Count Symmetric Integers.cpp

---
+++ Count Symmetric Integers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    bool confn(string str){
+        int n=str.length();
+        if(n%2)return false;
+        int sum=0;
+        for(int i=0;i<n/2;i++){
+            sum+=((str[i]-'0')-(str[n-1-i]-'0'));
+        }
+        return (sum==0);
+    }
+    int countSymmetricIntegers(int low, int high) {
+        int cnt=0;
+        for(int i=low;i<=high;i++){
+            if(confn(to_string(i))){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
