5c05adde412a919ef270816ae098d5271b64e482
Jash Hinger <93431632+jash459@users.noreply.github.com>
2025-05-04 10:37:41 +0530

    Create Number of Equivalent Domino Pairs.cpp

---
+++ Number of Equivalent Domino Pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+            int count = 0;
+            for (int i = 0 ; i < dominoes.size() ; i++) {
+                for (int j = i + 1 ; j < dominoes.size() ; j++) {
+                    int a = dominoes[i][0], b = dominoes[i][1];
+                    int c = dominoes[j][0], d = dominoes[j][1];
+                    if ((a == c && b == d) || (a == d && b == c))
+                    ++count;
+                }
+            }
+            return count;
+        }
+    };
