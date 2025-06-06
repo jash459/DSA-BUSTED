d0a3097e0562f1f2d7bd772ddb0b5e5a25bdc1a4
Jash Hinger <93431632+jash459@users.noreply.github.com>
2025-06-06 08:35:21 +0530

    Create Using a Robot to Print the Lexicographically Smallest String.cpp

---
+++ Using a Robot to Print the Lexicographically Smallest String.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string robotWithString(string s) {
+        vector<int> freq(26, 0);
+        int low = 0;
+        string p, t;
+
+        for (auto i : s) {
+            freq[i - 'a']++;
+        }
+
+        for (auto i : s) {
+            t += i;
+            freq[i - 'a']--;
+
+            while (low < 25 && freq[low] == 0) {
+                low++;
+            }
+
+            while (!t.empty() && low >= t.back() - 'a') {
+                p += t.back();
+                t.pop_back();
+            }
+        }
+
+        return p;
+    }
+};
