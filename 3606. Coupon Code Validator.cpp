b169f58dc8d7fed009fb7a31a95c49bb0defbf7a
Jash Hinger <93431632+jash459@users.noreply.github.com>
2025-12-13 08:50:01 +0530

    Create 3606. Coupon Code Validator.cpp

---
+++ 3606. Coupon Code Validator.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> validateCoupons(vector<string>& code,
+                                   vector<string>& businessLine,
+                                   vector<bool>& isActive) {
+        unordered_map<string, int> priority = {
+            {"electronics", 0},
+            {"grocery", 1},
+            {"pharmacy", 2},
+            {"restaurant", 3}
+        };
+
+        vector<pair<int, string>> valid;
+
+        for (int i = 0; i < code.size(); i++) {
+            if (isActive[i] &&
+                priority.count(businessLine[i]) &&
+                isValidCode(code[i])) {
+
+                valid.push_back({priority[businessLine[i]], code[i]});
+            }
+        }
+        sort(valid.begin(), valid.end());
+
+        vector<string> result;
+        for (auto &p : valid) {
+            result.push_back(p.second);
+        }
+
+        return result;
+    }
+
+private:
+    bool isValidCode(const string& s) {
+        if (s.empty()) return false;
+        for (char c : s) {
+            if (!isalnum(c) && c != '_')
+                return false;
+        }
+        return true;
+    }
+};
