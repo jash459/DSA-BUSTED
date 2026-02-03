2f80b51314c802aaec1a0366ede4bf79e4c6e4ef
Jash Hinger <93431632+jash459@users.noreply.github.com>
2026-02-03 08:14:13 +0530

    Create 3637. Trionic Array I.cpp

---
+++ 3637. Trionic Array I.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        int peak = -1;
+        int valley = n;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (peak == -1 && nums[i] >= nums[i + 1])
+                peak = i;
+            
+            if (valley == n && nums[n - 1 - i] <= nums[n - 2 - i])
+                valley = n - 1 - i;
+            
+            if (peak > -1 && valley < n) break;
+        }
+
+        if (peak < 1 || valley > n - 2)
+            return false;
+
+        for (int i = peak; i < valley; i++)
+            if (nums[i] <= nums[i + 1])
+                return false;
+
+        return true;
+    }
+};
