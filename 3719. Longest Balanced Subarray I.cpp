f4a826e27eee680dbb6a681ad1c0b654c48e710e
Jash Hinger <93431632+jash459@users.noreply.github.com>
2026-02-10 08:49:04 +0530

    Create 3719. Longest Balanced Subarray I.cpp

---
+++ 3719. Longest Balanced Subarray I.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static int longestBalanced(vector<int>& nums) {
+        const int n=nums.size();
+        unordered_map<int, short> freq;
+        freq.reserve(n);
+        int len=0;
+        for(int l=0; l<n; l++){
+            int cnt[2]={0};
+            freq.clear();
+            for(int r=l; r<n; r++){
+                const int x=nums[r];
+                if (++freq[x]==1) cnt[x&1]++;
+                if (cnt[0]==cnt[1])
+                    len=max(len, r-l+1);
+            }
+        }
+        return len;
+    }
+};
