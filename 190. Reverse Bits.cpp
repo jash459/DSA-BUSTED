c188eb2fbc12a14138f18359c6f7d2213c67667e
Jash Hinger <93431632+jash459@users.noreply.github.com>
2026-02-16 08:10:16 +0530

    Create 190. Reverse Bits.cpp

---
+++ 190. Reverse Bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+    if (n==0) return 0;
+    int ans=0;
+    for(; n; n&=(n-1)){
+        ans+=1<<(31-__builtin_ctz(n));
+    }
+    return ans;
+}
+};
