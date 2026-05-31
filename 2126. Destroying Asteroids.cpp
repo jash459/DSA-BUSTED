4d977c9463cf339c3dc90cab7efe215e889e19c8
Jash Hinger <93431632+jash459@users.noreply.github.com>
2026-05-31 10:50:07 +0530

    Create 2126. Destroying Asteroids.cpp

---
+++ 2126. Destroying Asteroids.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        long long currentMass = mass;
+        for (int asteroid : asteroids) {
+            if (currentMass < asteroid) {
+                return false;
+            }
+            currentMass += asteroid;
+        }
+        return true;
+    }
+};
